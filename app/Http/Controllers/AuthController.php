4862083c63ecb6b04a740ac0591d494d316192d2
dimsard <d.ardiansyah999@gmail.com>
2025-11-09 08:59:39 +0700

    firts commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'phone' => ['nullable', 'string', 'max:20'],
+            'departemen' => ['nullable', 'string', 'max:20'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
 
@@ -57,6 +58,7 @@ class AuthController extends Controller
             'name' => $validated['name'],
             'email' => $validated['email'],
             'phone' => $validated['phone'] ?? null,
+            'departemen' => $validated['departemen'] ?? null,
             'password' => Hash::make($validated['password']),
             'role' => 'user',
         ]);
